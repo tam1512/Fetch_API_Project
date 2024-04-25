0de0120f28373baa0202b16eeb3d3ecd56581712
TonThanhTam <tamtt1512@gmail.com>
2024-04-26 02:50:34 +0700

    create new access token when refresh token valid

--- js/app.js
+++ js/app.js
@@ -12,12 +12,13 @@ const app = {
             <label for="password">Mật khẩu</label>
             <input type="password" name="password" id="password" class="form-control" placeholder="Mật khẩu..." required>
          </div>
-         <button class="btn btn-primary btn-block">Đăng nhập</button>
+         <button type="submit" class="btn btn-primary btn-block btn-login">Đăng nhập</button>
       </form>
    </div>`,
    accountForm: `
    <div class="w-50 mx-auto">
       <h1 class="text-center">TÀI KHOẢN</h1>
+      <p>Xin chào: <b id="fullname-profile"></b> | <a href="#" class="logout">Đăng xuất</a></p>
       <form class="account-form">
          <div class="form-group">
             <label for="fullname">Họ và tên</label>
@@ -35,7 +36,7 @@ const app = {
             </div>
             <div id="avatar"></div>
          </div>
-         <button class="btn btn-primary btn-block">Lưu thay đổi</button>
+         <button type="submit" class="btn btn-primary btn-block">Lưu thay đổi</button>
       </form>
    </div>`,
    notify: (msg, msgType="success") => {
@@ -58,6 +59,14 @@ const app = {
             this.handleUpdateAccount(e.target);
          }
       })
+      root.addEventListener('click', (e) => {
+         if (!e.target.tagName.toLowerCase() === 'a') {
+            e.preventDefault();
+         }
+         if(e.target.classList.contains('logout')) {
+            this.handleLogout();
+         }
+      })
    },
    handleLogin: async function(form) {
       const data = Object.fromEntries([...new FormData(form)])
@@ -72,7 +81,7 @@ const app = {
       .then((validate) => {
          if(validate.status == 200) {
             localStorage.setItem('tokens', JSON.stringify(validate.data));
-            this.start();
+            this.start(true);
          } else {
             this.notify(validate.message, 'warning')
          }
@@ -93,7 +102,7 @@ const app = {
          .then(result => {
             if(result.status == 200) {
                this.notify("Lưu thông tin thành công");
-               this.start();
+               this.start(true);
             } else {
                this.notify("Thay đổi thông tin thất bại. Vui lòng kiểm tra lại", "warning")
             }
@@ -101,10 +110,10 @@ const app = {
       }
       
    },
-   getProfile: async function() {
+   handleLogout: async function () {
       if(localStorage.getItem('tokens')) {
          const {access_token: accessToken} = JSON.parse(localStorage.getItem('tokens'));
-         await fetch(`${this.serverApi}/auth/profile`, {
+         await fetch(`${this.serverApi}/auth/logout`, {
             method: 'GET',
             headers: {
                Authorization: 'Bearer ' + accessToken
@@ -112,24 +121,94 @@ const app = {
          })
          .then(res => res.json())
          .then((response) => {
-            if(response.status != 200) {
+            if(response.status == 200) {
                localStorage.removeItem('tokens');
-               this.start();
+               this.start(true);
             } else {
-               const {fullname, email, avatar} = response.data;
-
-               const fullnameEl = root.querySelector(".account-form #fullname");
-               const emailEl = root.querySelector(".account-form #email");
-               const avatarEl = root.querySelector(".account-form #avatar");
-
-               fullnameEl.value = fullname;
-               emailEl.value = email;
-               avatarEl.innerHTML = avatar ? `<img width=200 src="${avatar}"/>` : `<img width=200 src="https://vineview.com/wp-content/uploads/2017/07/avatar-no-photo-300x300.png"/>`;
+               this.notify('Đã có lỗi xảy ra vui lòng thử lại sau', 'warning');
             }
          })
       }
    },
-   start: function() {
+   getProfile: async function() {
+      if(localStorage.getItem('tokens')) {
+         const {access_token: accessToken} = JSON.parse(localStorage.getItem('tokens'));
+         const response = await fetch(`${this.serverApi}/auth/profile`, {
+            method: 'GET',
+            headers: {
+               Authorization: 'Bearer ' + accessToken
+            }
+         })
+         // .then(res => res.json())
+         // .then((response) => {
+         //    if(response.status != 200) {
+         //       let newToken = this.getNewAccessToken();
+         //       if(newToken) {
+         //          localStorage.setItem('tokens', newToken);
+         //          this.getProfile();
+         //       } else {
+         //          localStorage.removeItem('tokens');
+         //          this.start(false);
+         //       }
+         //    } else {
+         //       const {fullname, email, avatar} = response.data;
+
+         //       const fullnameProfileEl = root.querySelector("#fullname-profile");
+         //       const fullnameEl = root.querySelector(".account-form #fullname");
+         //       const emailEl = root.querySelector(".account-form #email");
+         //       const avatarEl = root.querySelector(".account-form #avatar");
+
+         //       fullnameProfileEl.textContent = fullname;
+         //       fullnameEl.value = fullname;
+         //       emailEl.value = email;
+         //       avatarEl.innerHTML = avatar ? `<img alt="" width=200 src="${avatar}"/>` : `<img alt="" width=200 src="https://vineview.com/wp-content/uploads/2017/07/avatar-no-photo-300x300.png"/>`;
+         //    }
+         // })
+
+         let data = await response.json();
+         if(response.ok) {
+            const {fullname, email, avatar} = data.data;
+
+            const fullnameProfileEl = root.querySelector("#fullname-profile");
+            const fullnameEl = root.querySelector(".account-form #fullname");
+            const emailEl = root.querySelector(".account-form #email");
+            const avatarEl = root.querySelector(".account-form #avatar");
+
+            fullnameProfileEl.textContent = fullname;
+            fullnameEl.value = fullname;
+            emailEl.value = email;
+            avatarEl.innerHTML = avatar ? `<img alt="" width=200 src="${avatar}"/>` : `<img alt="" width=200 src="https://vineview.com/wp-content/uploads/2017/07/avatar-no-photo-300x300.png"/>`;
+         } else {
+            const newToken = await this.getNewAccessToken();
+            if(newToken) {
+               localStorage.setItem('tokens', JSON.stringify(newToken));
+               this.getProfile();
+            } else {
+               localStorage.removeItem('tokens');
+               this.start(false);
+            }
+         }
+      }
+   },
+   getNewAccessToken: async function () {
+      if(localStorage.getItem('tokens')) {
+         const {refresh_token: refreshToken} = JSON.parse(localStorage.getItem('tokens'));
+         const response = await fetch(`${this.serverApi}/auth/refresh-token`, {
+            method: 'POST',
+            headers: {
+               "Content-type" : "application/json"
+            },
+            body: JSON.stringify({refresh_token : refreshToken})
+         })
+         let data = await response.json();
+         if(response.ok) {
+            return data.data;
+         } else {
+            return false;
+         }
+      }
+   },
+   start: function(old=false) {
       const isLogin = localStorage.getItem('tokens') ? true : false;
       if(!isLogin) {
          html = this.loginForm
@@ -137,7 +216,9 @@ const app = {
          html = this.accountForm
       }
       this.render(html);
-      this.addEvent();
+      if(!old) {
+         this.addEvent();
+      }
       this.getProfile();
    }
 }
