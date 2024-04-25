66d2c3bfee4d429578eea57c7acc7d08d1de5d6b
TonThanhTam <tamtt1512@gmail.com>
2024-04-25 16:49:49 +0700

    getProfile and updateProfile

--- js/app.js
+++ js/app.js
@@ -28,8 +28,12 @@ const app = {
             <input type="email" name="email" id="email" class="form-control" placeholder="Email...">
          </div>
          <div class="form-group">
-            <label for="avatar">Ảnh đại diện</label>
-            <input type="file" name="avatar" id="avatar" class="form-control" placeholder="Ảnh đại diện...">
+            <label for="inputGroupAvatar">Ảnh đại diện</label>
+            <div class="custom-file mb-2">
+               <label class="custom-file-label" for="inputGroupAvatar">Choose file</label>
+               <input type="file" name="avatar" class="custom-file-input mb-2" id="inputGroupAvatar"  placeholder="Ảnh đại diện...">
+            </div>
+            <div id="avatar"></div>
          </div>
          <button class="btn btn-primary btn-block">Lưu thay đổi</button>
       </form>
@@ -50,11 +54,14 @@ const app = {
          if(e.target.classList.contains('login-form')) {
             this.handleLogin(e.target);
          }
+         if(e.target.classList.contains('account-form')) {
+            this.handleUpdateAccount(e.target);
+         }
       })
    },
    handleLogin: async function(form) {
       const data = Object.fromEntries([...new FormData(form)])
-      const response = await fetch(`${this.serverApi}/auth/login`, {
+      await fetch(`${this.serverApi}/auth/login`, {
          method: 'POST',
          headers: {
             'Content-Type': 'application/json'
@@ -71,6 +78,57 @@ const app = {
          }
       })
    },
+   handleUpdateAccount: async function(form) {
+      if(localStorage.getItem('tokens')) {
+         const {access_token: accessToken} = JSON.parse(localStorage.getItem('tokens'));
+         const data = new FormData(form);
+         await fetch(`${this.serverApi}/auth/profile`, {
+            method: 'POST',
+            headers: {
+               Authorization: 'Bearer ' + accessToken
+            },
+            body: data
+         })
+         .then(res => res.json())
+         .then(result => {
+            if(result.status == 200) {
+               this.notify("Lưu thông tin thành công");
+               this.start();
+            } else {
+               this.notify("Thay đổi thông tin thất bại. Vui lòng kiểm tra lại", "warning")
+            }
+         })
+      }
+      
+   },
+   getProfile: async function() {
+      if(localStorage.getItem('tokens')) {
+         const {access_token: accessToken} = JSON.parse(localStorage.getItem('tokens'));
+         await fetch(`${this.serverApi}/auth/profile`, {
+            method: 'GET',
+            headers: {
+               Authorization: 'Bearer ' + accessToken
+            }
+         })
+         .then(res => res.json())
+         .then((response) => {
+            if(response.status != 200) {
+               localStorage.removeItem('tokens');
+               this.start();
+            } else {
+               const {fullname, email, avatar} = response.data;
+
+               const fullnameEl = root.querySelector(".account-form #fullname");
+               const emailEl = root.querySelector(".account-form #email");
+               const avatarEl = root.querySelector(".account-form #avatar");
+
+               fullnameEl.value = fullname;
+               emailEl.value = email;
+               avatarEl.innerHTML = avatar ? `<img width=200 src="${avatar}"/>` : `<img width=200 src="https://vineview.com/wp-content/uploads/2017/07/avatar-no-photo-300x300.png"/>`;
+            }
+         })
+      }
+   },
    start: function() {
       const isLogin = localStorage.getItem('tokens') ? true : false;
       if(!isLogin) {
@@ -80,6 +138,7 @@ const app = {
       }
       this.render(html);
       this.addEvent();
+      this.getProfile();
    }
 }
 const root = document.querySelector('#root');
